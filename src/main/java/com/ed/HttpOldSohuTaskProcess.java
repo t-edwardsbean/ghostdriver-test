b9401f5fc772f2bdd86b9da3de795ac3596ec377
edwardsbean@gmail.com <edwardsbean@gmail.com>
2015-03-02 15:19:11 +0800

    完成Old重试

--- src/main/java/com/ed/HttpOldSohuTaskProcess.java
+++ src/main/java/com/ed/HttpOldSohuTaskProcess.java
@@ -45,14 +45,61 @@ public class HttpOldSohuTaskProcess extends TaskProcess {
         //检查邮箱
         HttpGet checkEmail = new HttpGet("http://i.sohu.com/login/checksname?cn=" + task.getEmail());
         checkEmail.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
-        checkEmail.addHeader("Referer","http://i.sohu.com/login/reg.do");
+        checkEmail.addHeader("Referer", "http://i.sohu.com/login/reg.do");
         CloseableHttpResponse checkEmailResponse = HttpUtils.httpclient.execute(checkEmail, context);
         String checkEmailResult = EntityUtils.toString(checkEmailResponse.getEntity());
         String checkResult = JsonPath.read(checkEmailResult, "$.msg");
         log.debug("检查邮箱名：" + checkResult);
 
 
-        //获取图片验证码
+        while (true) {
+
+            //获取图片验证码
+            String code = getPictureCode(context);
+            //提交
+            HttpPost submitEmail = new HttpPost("http://i.sohu.com/login/sreg.do?_input_encode=utf-8");
+            checkEmail.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
+            checkEmail.addHeader("Referer", "http://i.sohu.com/login/reg.do");
+            checkEmail.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            checkEmail.addHeader("Origin", "http://i.sohu.com");
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            nvps.add(new BasicNameValuePair("bru", ""));
+            nvps.add(new BasicNameValuePair("default_page", ""));
+            nvps.add(new BasicNameValuePair("from", ""));
+            nvps.add(new BasicNameValuePair("sappId", ""));
+            nvps.add(new BasicNameValuePair("source", ""));
+            nvps.add(new BasicNameValuePair("user", task.getEmail()));
+            nvps.add(new BasicNameValuePair("nickname", task.getEmail()));
+            nvps.add(new BasicNameValuePair("passwd", task.getPassword()));
+            nvps.add(new BasicNameValuePair("vcode", code));
+            nvps.add(new BasicNameValuePair("vcodeEn", ""));
+            nvps.add(new BasicNameValuePair("agree", "on"));
+            //中文url编码
+            submitEmail.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+            CloseableHttpResponse submitEmailResponse = HttpUtils.httpclient.execute(submitEmail, context);
+            String submitEmailResult = EntityUtils.toString(submitEmailResponse.getEntity());
+            for (Cookie cookie : cookieStore.getCookies()) {
+                if (cookie.getName().equals("errorcode")) {
+                    if (cookie.getValue().contains("0001")) {
+                        log.error("验证码错误");
+                        continue;
+                    }
+                    log.error("注册失败：" + cookie.getValue());
+                    return;
+                }
+            }
+            if (submitEmailResult.contains("您的注册数量已超过正常限制,请使用已有账号进行登录")) {
+                log.error("您的注册数量已超过正常限制,请使用已有账号进行登录");
+                return;
+            } else {
+                log.info("注册成功");
+                return;
+            }
+        }
+
+    }
+
+    public String getPictureCode(HttpClientContext context) throws Exception {
         String code;
         while (true) {
             String path = "code.png";
@@ -73,43 +120,8 @@ public class HttpOldSohuTaskProcess extends TaskProcess {
             if ("-1008".equals(code)) {
                 Thread.sleep(1000);
             } else {
-                break;
+                return code;
             }
-        }
-
-        //提交
-        HttpPost submitEmail = new HttpPost("http://i.sohu.com/login/sreg.do?_input_encode=utf-8");
-        checkEmail.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
-        checkEmail.addHeader("Referer", "http://i.sohu.com/login/reg.do");
-        checkEmail.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        checkEmail.addHeader("Origin", "http://i.sohu.com");
-        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        nvps.add(new BasicNameValuePair("bru", ""));
-        nvps.add(new BasicNameValuePair("default_page", ""));
-        nvps.add(new BasicNameValuePair("from", ""));
-        nvps.add(new BasicNameValuePair("sappId", ""));
-        nvps.add(new BasicNameValuePair("source", ""));
-        nvps.add(new BasicNameValuePair("user", task.getEmail()));
-        nvps.add(new BasicNameValuePair("nickname", task.getEmail()));
-        nvps.add(new BasicNameValuePair("passwd", task.getPassword()));
-        nvps.add(new BasicNameValuePair("vcode", code));
-        nvps.add(new BasicNameValuePair("vcodeEn", ""));
-        nvps.add(new BasicNameValuePair("agree", "on"));
-        //中文url编码
-        submitEmail.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-        CloseableHttpResponse submitEmailResponse = HttpUtils.httpclient.execute(submitEmail, context);
-        String submitEmailResult = EntityUtils.toString(submitEmailResponse.getEntity());
-        for (Cookie cookie : cookieStore.getCookies()) {
-            if (cookie.getName().equals("errorcode")) {
-                log.error("注册失败：" + cookie.getValue());
-                return;
-            }
-        }
-        if (submitEmailResult.contains("您的注册数量已超过正常限制,请使用已有账号进行登录")) {
-            log.error("您的注册数量已超过正常限制,请使用已有账号进行登录");
-            return;
-        } else {
-            log.info("注册成功");
         }
     }
 
@@ -124,7 +136,7 @@ public class HttpOldSohuTaskProcess extends TaskProcess {
         registryMachine.thread(1);
         registryMachine.setTaskProcess(new HttpOldSohuTaskProcess(""));
         registryMachine.addTask(
-                new Task("aazx123amm18", "2692194")
+                new Task("aazx123amm20", "2692194")
         );
         boolean status = UUAPI.checkAPI();    //校验API，必须调用一次，校验失败，打码不成功
 
